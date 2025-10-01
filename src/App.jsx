b56c6d3035efd342f244cab30ca3a72242dd7365
RuVerr <ruvermishyan@gmail.com>
2025-10-02 00:18:58 +0400

    a loading screen has been added at the first login so that all components are loaded and the HomeHeader itself is used since there is animation on the touch if the download is long, then he can play with it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 import AppRouter from "./Router/AppRouter";
+import ScreenLoader from "./Components/ScreenLoader/ScreenLoader";
 
 function App() {
-  return <AppRouter />;
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    const handleLoader = () => setLoader(false);
+
+    addEventListener("load", handleLoader);
+
+    return () => window.removeEventListener("load", handleLoader);
+  }, []);
+
+  return <>{loader ? <ScreenLoader /> : <AppRouter />}</>;
 }
 
 export default App;
---
+++ src/Components/ScreenLoader/ScreenLoader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import WithStyloSpan from "../WithStyloSpan/WithStyloSpan";
+
+import "./screenLoader.scss";
+import HomeHeader from "../Header/HomeHeader/HomeHeader";
+
+export default function ScreenLoader() {
+  return (
+    <div className="screenLoader">
+      <HomeHeader />
+    </div>
+  );
+}
