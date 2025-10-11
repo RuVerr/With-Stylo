be907579f3cb46d96c49e71bcc42dbb7b1bd5c51
RuVerr <ruvermishyan@gmail.com>
2025-10-11 14:50:34 +0400

    fix: correct bgFocus import case

--- src/pages/Documentation/DocumentationBar/DocumentationBar.jsx
+++ src/pages/Documentation/DocumentationBar/DocumentationBar.jsx
@@ -6,7 +6,7 @@ import { useDocsContent } from "../../../Store/DocumentationContent/Documentatio
 import sideBarIcon from "../../../images/Icon/sideBarIcon.png";
 
 import "./documentationBar.scss";
-import BgFocus from "../../../Components/bgFocus/bgfocus";
+import BgFocus from "../../../Components/bgFocus/bgFocus";
 
 export default function DocumentationBar() {
   const setActiveId = useDocsContent((state) => state.setActiveId);
