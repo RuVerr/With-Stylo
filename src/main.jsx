f19e3d758319328da2e853ff08f5857639e3949a
RuVerr <ruvermishyan@gmail.com>
2025-10-11 14:18:05 +0400

    Fixed paths for netflie

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// src/main.jsx
+import React from "react";
+import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import './index.scss'
+import "./index.scss";
 
-createRoot(document.getElementById('root')).render(
+// Лоадер из HTML
+window.addEventListener("load", () => {
+  const loader = document.getElementById("screenLoader");
+  if (loader) {
+    loader.remove();
+  }
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-
-  window.addEventListener("load", () =>{
-    const loader = document.getElementById("screenLoader");
-    if(loader){
-      loader.remove()
-    }
-  })
-)
+  </StrictMode>
+);
